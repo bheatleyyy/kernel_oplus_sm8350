a125d9f73dae2422596331cd42e037fefd49eedf
Adrian Salido <salidoa@google.com>
2017-12-04 16:52:17 -0800

    techpack: display: drm/msm: add idle state sysfs node
    Add a sysfs mechanism to track the idle state of display subsystem.
    This allows user space to poll on the idle state node to detect when
    display goes idle for longer than the time set.
    
    Bug: 139655049
    Bug: 126304228
    
    DennySPb: adapt to sm8350
       * Change drm_encoder mask to 2
    
    Change-Id: I21e3c7b0830a9695db9f65526c111ce5153d1764
    Signed-off-by: Adrian Salido <salidoa@google.com>
    Signed-off-by: Robb Glasser <rglasser@google.com>
    (cherry picked from commit 11a2193b434cb3130743fbff89a161062883132e)
    Signed-off-by: Ken Huang <kenbshuang@google.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -1656,6 +1656,7 @@ static void _sde_encoder_rc_kickoff_delayed(struct sde_encoder_virt *sde_enc,
 		_sde_encoder_rc_cancel_delayed(sde_enc, sw_event);
 	else
 		_sde_encoder_rc_restart_delayed(sde_enc, sw_event);
+
 }
 
 static int _sde_encoder_rc_kickoff(struct drm_encoder *drm_enc,
@@ -1707,6 +1708,8 @@ end:
 	if (!sde_enc->delay_kickoff)
 		_sde_encoder_rc_kickoff_delayed(sde_enc, sw_event);
 
+	msm_idle_set_state(drm_enc, false);
+
 	mutex_unlock(&sde_enc->rc_lock);
 	return ret;
 }
@@ -1716,6 +1719,8 @@ static int _sde_encoder_rc_pre_stop(struct drm_encoder *drm_enc,
 {
 	/* cancel delayed off work, if any */
 	_sde_encoder_rc_cancel_delayed(sde_enc, sw_event);
+
+	msm_idle_set_state(drm_enc, true);
 
 	mutex_lock(&sde_enc->rc_lock);
 
